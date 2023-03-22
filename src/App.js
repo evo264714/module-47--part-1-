686d83561e6872bbfdc6fa5488628b3365949838
evo264714 <mh264714@gmail.com>
2023-03-22 19:37:56 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singers = [
+  {name: 'Dr. Mahfuz', job: 'Singer'},
+  {name: 'Eva Rahman', job: 'Singer2'},
+  {name: 'Agun', job: 'Singer3'},
+  {name: 'Shuvro', job: 'Singer4'},
+  
+]
+
+
 function App() {
+  const nayoks = ['Rubel', 'Sakib', 'Mushfiq', 'Jashim', 'Salman', 'Riyaz', 'Razzak']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+
+      {/* {
+        nayoks.map(nayok =><Person name={nayok}></Person>)
+      } */}
+
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} hero="Rubel2"></Person>
+      <Person name={nayoks[1]} hero="Sakib2"></Person>
+      <Person name={nayoks[2]} hero="Mushfiq2"></Person> */}
+      
+      <h5>New Component</h5>
+      <p>React is here!</p>
+
+      <Friend movie="movie1" phone= "017777"></Friend>
+      <Friend phone="019999"></Friend>
     </div>
   );
+}
+
+function Person(props){
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>{props.hero}</p>
+  </div>
+  );
+}
+
+function Friend (props){
+  console.log(props);
+  return (
+    <div className='container'>
+      <h3>Name: {props.movie}</h3>
+      <p>phone: {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
